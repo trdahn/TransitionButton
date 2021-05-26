1e7d08e338d0a8737476b580c43cc6a4b043c99c
Danh Hung <devcon.cloud@gmail.com>
2021-05-26 19:41:17 +0700

    Support SPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,24 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-  name: "TransitionButton"
+    name: "TransitionButton",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library (
+            name: "TransitionButton",
+            targets: ["TransitionButton"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "TransitionButton",
+            path: "Source",
+            exclude: ["Info.plist"],
+            linkerSettings: [
+                .linkedFramework("UIKit")
+            ]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
